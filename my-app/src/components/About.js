011d0cfcc086ca436f7abecefd1eb75572278360
anu-shree <anupriyashree34@gmail.com>
2019-07-15 01:49:57 +0530

    final assignment

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,34 +1,55 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
-function RenderLeader ({leaders}) {
-    return (
-              <div key={leaders.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={leaders.image} alt={leaders.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{leaders.name}</Media>
-                    <p>{leaders.description}</p>
-                  </Media>
-                </Media>
-              </div>
-            );
-}
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function About(props) {
 
-    const leaders = props.leaders.map((leaders) => {
+    const leaders = props.leaders.leaders.map(leader => {
         return (
-            <div className="col-12 m-1"  key={leaders.id}>
-                <RenderLeader leaders={leaders} />
-            </div>
+            <RenderLeader leader={leader} />
         );
     });
 
-    return(
+    function RenderLeader({ leader }) {
+        return (
+            <Fade in>
+                <Media tag="li" key={leader.id} className="col-12 mt-5" >
+                    <Media left middle>
+                        <Media object src={baseUrl + leader.image} alt={leader.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+                </Media>
+            </Fade>
+        );
+    }
+
+    function RenderLeaders() {
+
+        if (props.leaders.isLoading) {
+            return <Loading />;
+        }
+        else if (props.leaders.errMess) {
+            return (
+                <h4>{props.leaders.errMess}</h4>
+            );
+        }
+        else return (
+            <Media list>
+                <Stagger in>
+                    {leaders}
+                </Stagger>
+            </Media>
+        );
+    }
+
+    return (
         <div className="container">
             <div className="row">
                 <Breadcrumb>
@@ -38,7 +59,7 @@ function About(props) {
                 <div className="col-12">
                     <h3>About Us</h3>
                     <hr />
-                </div>                
+                </div>
             </div>
             <div className="row row-content">
                 <div className="col-12 col-md-6">
@@ -78,18 +99,15 @@ function About(props) {
                     </Card>
                 </div>
             </div>
-            <div className="container m-1">
-                <div className="row m-1">
+            <div className="row row-content">
+                <div className="col-12">
                     <h2>Corporate Leadership</h2>
                 </div>
-                <div className="row">
-                    <Media list>
-                        {leaders}
-                    </Media>
+                <div className="col-12">
+                    <RenderLeaders />
                 </div>
             </div>
         </div>
     );
 }
-
 export default About;    
--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -1,30 +1,50 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay,
-    CardTitle, Breadcrumb, BreadcrumbItem, } from 'reactstrap';
-    import {Link} from 'react-router-dom';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
-    function RenderMenuItem ({dish}) {
+function RenderMenuItem({ dish, onClick }) {
+    return (
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
+        </Card>
+    );
+}
+
+const Menu = props => {
+    const menu = props.dishes.dishes.map(dish => {
         return (
-            <Card>
-                <Link to={`/menu/${dish.id}`} >
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Link>
-            </Card>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RenderMenuItem dish={dish} />
+            </div>
+        )
+    });
+
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
         );
     }
-    const Menu = (props) => {
-
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} />
+    else if (props.dishes.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMess}</h4>
                 </div>
-            );
-        });
-
+            </div>
+        );
+    }
+    else
         return (
             <div className="container">
                 <div className="row">
@@ -35,13 +55,13 @@ import { Card, CardImg, CardImgOverlay,
                     <div className="col-12">
                         <h3>Menu</h3>
                         <hr />
-                    </div>                
+                    </div>
                 </div>
                 <div className="row">
                     {menu}
                 </div>
             </div>
         );
-    }
+}
 
 export default Menu;
